3e8f1c95c50ac4faea9d38753b7c8d7d112d7044
Łukasz Majewski <lukasz.majewski@sumito.uk>
2021-12-22 16:15:22 +0100

    Fix BytesEncodable

--- Sources/Solana/Extensions/BytesEncodable.swift
+++ Sources/Solana/Extensions/BytesEncodable.swift
@@ -5,7 +5,7 @@ public protocol BytesEncodable {
 }
 
 extension UInt8: BytesEncodable {
-    var bytes: [UInt8] { [self] }
+    public var bytes: [UInt8] { [self] }
 }
 
 extension UInt64: BytesEncodable {}
@@ -17,17 +17,17 @@ extension PublicKey: BytesEncodable {}
 extension Data: BytesEncodable {}
 
 extension Bool: BytesEncodable {
-    var bytes: [UInt8] {self ? [UInt8(1)]: [UInt8(0)]}
+    public var bytes: [UInt8] {self ? [UInt8(1)]: [UInt8(0)]}
 }
 
 extension Array: BytesEncodable where Element == BytesEncodable {
-    var bytes: [UInt8] {reduce([], {$0 + $1.bytes})}
+    public var bytes: [UInt8] {reduce([], {$0 + $1.bytes})}
 }
 
 extension RawRepresentable where RawValue == UInt32 {
-    var bytes: [UInt8] {rawValue.bytes}
+    public var bytes: [UInt8] {rawValue.bytes}
 }
 
 extension RawRepresentable where RawValue == UInt8 {
-    var bytes: [UInt8] {rawValue.bytes}
+    public var bytes: [UInt8] {rawValue.bytes}
 }
