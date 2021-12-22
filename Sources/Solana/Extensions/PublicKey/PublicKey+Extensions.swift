b447567d1a68eb6ef084eb54e7cf6534e3e4a8f8
Łukasz Majewski <lukasz.majewski@sumito.uk>
2021-12-22 14:27:42 +0100

    Add sysvarClock

--- Sources/Solana/Extensions/PublicKey/PublicKey+Extensions.swift
+++ Sources/Solana/Extensions/PublicKey/PublicKey+Extensions.swift
@@ -3,6 +3,7 @@ import Foundation
 public extension PublicKey {
     static let tokenProgramId = PublicKey(string: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")!
     static let sysvarRent = PublicKey(string: "SysvarRent111111111111111111111111111111111")!
+    static let sysvarClock = PublicKey(string: "SysvarC1ock11111111111111111111111111111111")!
     static let programId = PublicKey(string: "11111111111111111111111111111111")!
     static let wrappedSOLMint = PublicKey(string: "So11111111111111111111111111111111111111112")!
     static let ownerValidationProgramId = PublicKey(string: "4MNPdKu9wFMvEeZBMt3Eipfs5ovVWTJb31pEXDJAAxX5")!
